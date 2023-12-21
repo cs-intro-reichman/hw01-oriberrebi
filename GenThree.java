27b168b06f69c35ed2f29bea4f1b183cfef2ce15
OriB <ori.berrebi@runi.ac.il>
2023-12-21 17:54:40 +0200

    Add awesome code for Homework 1

--- GenThree.java
+++ GenThree.java
@@ -1,6 +1,6 @@
 import java.util.concurrent.ThreadLocalRandom;
 
-class GenThree {
+public class GenThree {
 public static void main(String args[]) {
 	Integer a = Integer.parseInt(args[0]);
 	Integer b = Integer.parseInt(args[1]);
